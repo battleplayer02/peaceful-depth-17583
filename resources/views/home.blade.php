23131251b5e358e37c991506d9f4b42420a888cd
Administrator <himanshu00qwerty@gmail.com>
2020-06-17 18:26:31 +0530

    dafdf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -207,35 +207,29 @@
     <!-- corona count section ending here -->
 
 
-    <!-- G-Map Section Start Here -->
-    <div class="gmaps-section">
-        <div class="map-area">
-            <div class='tableauPlaceholder' id='viz1592223387681' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;BI&#47;BIAnalystSessionDay12&#47;COVID-19Stats&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='path' value='views&#47;BIAnalystSessionDay12&#47;COVID-19Stats?:embed=y&amp;:display_count=y' /> <param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;BI&#47;BIAnalystSessionDay12&#47;COVID-19Stats&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1592223387681');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else { vizElement.style.width='100%';vizElement.style.height='727px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
-        </div>
-    </div>
-    <!-- G-Map Section Ending Here -->
-    <div align="center">
-        <div class="col-sm-6 col-md-6 col-lg-6" data-groups="[&quot;Locations&quot;,&quot;India&quot;]">
-            <div class='tableauPlaceholder' id='viz1585369681834' style='position: relative'><noscript><a href='#'><img
+    <div class="col-lg-12 col-12 mb-6 shuffle-item" data-groups="[&quot;World&quot;]">
+        <div class="position-relative rounded hover-wrapper">
+            <div class='tableauPlaceholder' id='viz1585339255136' style='position: relative'><noscript><a href='#'><img
                             alt=' '
-                            src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;N2&#47;N2ZF5RTQN&#47;1_rss.png'
+                            src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;T7&#47;T797P95WD&#47;1_rss.png'
                             style='border: none' /></a></noscript><object class='tableauViz' style='display:none;'>
                     <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
                     <param name='embed_code_version' value='3' />
-                    <param name='path' value='shared&#47;N2ZF5RTQN' />
+                    <param name='path' value='shared&#47;T797P95WD' />
                     <param name='toolbar' value='yes' />
                     <param name='static_image'
-                           value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;N2&#47;N2ZF5RTQN&#47;1.png' />
+                           value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;T7&#47;T797P95WD&#47;1.png' />
                     <param name='animate_transition' value='yes' />
                     <param name='display_static_image' value='yes' />
                     <param name='display_spinner' value='yes' />
                     <param name='display_overlay' value='yes' />
                     <param name='display_count' value='yes' /></object></div>
             <script
-                type='text/javascript'>                    var divElement = document.getElementById('viz1585369681834'); var vizElement = divElement.getElementsByTagName('object')[0]; vizElement.style.width = '100%'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; var scriptElement = document.createElement('script'); scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'; vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
-        </div>
+                type='text/javascript'>                    var divElement = document.getElementById('viz1585339255136'); var vizElement = divElement.getElementsByTagName('object')[0]; if (divElement.offsetWidth > 800) { vizElement.style.width = '100%'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; } else if (divElement.offsetWidth > 500) { vizElement.style.width = '100%'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; } else { vizElement.style.width = '100%'; vizElement.style.height = '927px'; } var scriptElement = document.createElement('script'); scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'; vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
         </div>
     </div>
+    </div>
+
 
 
 @stop
