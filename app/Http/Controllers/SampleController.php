d7322e33fbf19686b6d7ac37e9eaa7154e5b2ab2
Administrator <himanshu00qwerty@gmail.com>
2020-06-17 17:26:25 +0530

    bj

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -8,9 +8,10 @@ class SampleController extends Controller
 {
     //
     function dbCheck(){
-        echo("<pre>");
-        $users = DB::table("user_details")->get();
-        echo($users);
-        echo("</pre>");
+//        echo("<pre>");
+//        $users = DB::table("user_details")->get();
+//        echo($users);
+//        echo("</pre>");
+        echo("gggg");
     }
 }
