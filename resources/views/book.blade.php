2ae2ab1245a0ed1f7a3ef7aad0e52f842bab571d
Administrator <himanshu00qwerty@gmail.com>
2020-06-18 19:20:30 +0530

    style

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -46,11 +46,42 @@
         </div>
     </div>
     <div class="card">
-        <h5 class="card-header">Featured</h5>
+        <h5 class="card-header">
+            Book Appointment
+        </h5>
         <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <form>
+                <div class="form-group row">
+                    <label for="text1" class="col-2 col-form-label">Text Field</label>
+                    <div class="col-10">
+                        <input id="text1" name="text1" type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="text" class="col-2 col-form-label">Text Field</label>
+                    <div class="col-10">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text">
+                                    <i class="fa fa-address-card"></i>
+                                </div>
+                            </div>
+                            <input id="text" name="text" type="text" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="text2" class="col-2 col-form-label">Text Field</label>
+                    <div class="col-10">
+                        <input id="text2" name="text2" type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="offset-2 col-10">
+                        <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 @stop
