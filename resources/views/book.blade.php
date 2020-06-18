abb0d5456c1ff96aeabd108139572bbaa400bf56
Administrator <himanshu00qwerty@gmail.com>
2020-06-18 18:53:11 +0530

    dsa

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+{{--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">--}}
+{{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">--}}
 
 <form>
     <div class="form-group row">
