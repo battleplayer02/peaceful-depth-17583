69778cfb58f58051c0fad811fd9c24b957d0589e
Administrator <himanshu00qwerty@gmail.com>
2020-06-18 14:13:11 +0530

    styling

--- resources/views/registeruser.blade.php
+++ resources/views/registeruser.blade.php
@@ -1,111 +1,188 @@
-<html>
-    <head>
-        <meta name="csrf-param" content="authenticity_token" />
-        <meta name="csrf-token" content="eJp8A5GIe8tMrhLZzOKZtiXnN4CTRg9Ahmq07pucB8lHXQphVycCBctGUW33oh4rk4sXr/uLSBC2QnI9Tr2FCg==" />
-        <link rel="stylesheet" media="all" href="http://floating-earth-19478.herokuapp.com/assets/application-a4b917e3c310a38959ed9494d53faf9df577d0f5d79b9b2ab9c4cb9dbc42abd8.css" data-turbolinks-track="reload" />
-        <script src="http://floating-earth-19478.herokuapp.com/assets/application-2533c5099d25f4b1fef8883f00a85ff34d801828383870645cb2e1e88fb66731.js" data-turbolinks-track="reload"></script>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@extends('layout')
+@section('title')
+    <style>
+        .centerImage
+        {
+            align-self: center;
+            margin-bottom: 2%;
+            margin-left: 45%;
+        }
+        #over img {
+            margin-left: auto;
+            margin-right: auto;
+            margin-bottom: 1%;
+            display: block;
+        }
+        nav .nav-wrapper {
+            padding-left: 10px;
+        }
 
+        .container {
+            margin: 0px;
+            border: 1px solid #B22222;
+            max-width: 100%;
+            width: 100%;
+        }
 
-        <link href="{{ URL::asset('css/useruploadimage.css') }}" rel="stylesheet" type="text/css" >
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <style type="text/css">
-            .centerImage
-            {
-                align-self: center;
-                margin-bottom: 2%;
-                margin-left: 45%;
+        @media only screen and (min-width: 601px) {
+            .container {
+                width: 100%;
+                margin: 0px;
+                padding: 0px;
             }
-            #over img {
-                margin-left: auto;
-                margin-right: auto;
-                margin-bottom: 1%;
-                display: block;
+        }
+
+        @media only screen and (min-width: 993px) {
+            .container {
+                width: 100%;
+                margin: 0px;
             }
-            </style>
-    </head>
+        }
 
-    <body>
-        <h1 style="text-align: center">User Registration</h1>
-        <!-- Upload  -->
 
-                <form action="reguser" method="post" enctype="multipart/form-data" >
-                    {{ @csrf_field() }}
-                    <div id="over" style="width:100%; height:100%">
-                        <img class="img-responsive img-rounded centerImage" width="150" height="150" name="output" id="output">
-                    </div>
+        @media only screen and (max-width: 992px) {
+            .input-field .prefix ~ input {
+                margin: 0px;
 
-                    <label class="waves-effect waves-light btn centerImage" >
-                        Upload Photo
-                        <input type="file" name="user_profile" value="Upload Photo" style="width: 0px;height: 0px;overflow: hidden;" onchange="loadfile(event)">
-                    </label><br>
-                    @error('user_profile')
-                        <span style="color: red;"> {{ $message}}</span>
-                    @enderror
-                    <div class="row">
-                        <div class="input-field col s12  l6">
-                            <input type="text" name="fullname"><br>
-                            @error('fullname')
-                               <span style="color: red"> {{ $message}}</span>
-                            @enderror
-                            <label for="first_name">Full Name</label>
-                          </div>
-                          <div class="input-field col s12 l6">
-                            <input type="text" name="email"><br>
-                            @error('email')
-                                <span style="color: red">{{ $message}}</span>
-                            @enderror
-                            <label for="last_name">Email ID</label>
-                          </div>
-                          <div class="input-field col s12 l6">
-                            <input type="text" name="blood_group"><br>
-                            @error('blood_group')
-                                <span style="color: red">{{ $message}}</span>
-                            @enderror
-                            <label for="last_name">Blood Group</label>
-                          </div>
-                          <div class="input-field col s12 l6">
-                            <input type="text" name="contact_number"><br>
-                            @error('contact_number')
-                                <span style="color: red">{{ $message}}</span>
-                            @enderror
-                            <label for="last_name">Contact Number</label>
-                          </div>
-                          <div class="input-field col s12 ">
-                            <input type="text" name="gender"><br>
-                                @error('gender')
-                                    <span style="color: red">{{ $message}}</span>
-                                @enderror
-                            <label for="last_name">Gender</label>
-                          </div>
-                          <div class="input-field col s12 l6">
-                            <input type="password" name="password" ><br>
-                                @error('password')
-                                    <span style="color: red">{{ $message}}</span>
-                                @enderror
-                            <label for="last_name">Password</label>
-                          </div>
-                          <div class="input-field col s12 l6">
-                            <input type="password" name="confirm_password"><br>
-                                @error('confirm_password')
-                                    <span style="color: red">{{ $message}}</span>
-                                @enderror
-                            <label for="last_name">Confirm Password</label>
-                          </div>
-                          <button type="submit"  class="waves-effect waves-light btn">Submit</a>
-                    </div>
+                width: calc(100% - 3rem);
+            }
+        }
 
-                </form>
-            <script>
-                var loadfile= function(event)
-                {
-                    var output=document.getElementById("output");
-                    output.src= URL.createObjectURL(event.target.files[0]);
-                };
+        @media only screen and (max-width: 600px) {
+            .input-field .prefix ~ input {
+                width: 80%;
+            }
+        }
 
-            </script>
-    </body>
-</html>
+
+        h1 {
+            display: block;
+            font-size: 4em;
+            font-weight: bold;
+        }
+
+        .row {
+            padding-left: 50px;
+            padding-right: 50px;
+        }
+
+
+        .btn {
+            background-color: #2196f3;
+        &:hover {
+            background-color: #64b5f6;
+        }
+
+        }
+
+        .reset {
+
+        .row {
+            padding-left: 11px;
+            padding-right: 11px;
+        }
+
+        }
+
+        .dropdown-content li:hover, .dropdown-content li.active, .dropdown-content li.selected {
+            background-color: #eee;
+        }
+
+        .dropdown-content li.active.selected {
+            background-color: #e1e1e1;
+        }
+
+        .dropdown-content li.divider {
+            min-height: 0;
+            height: 1px;
+        }
+
+        .dropdown-content li > a, .dropdown-content li > span {
+            font-size: 26px;
+            color: black;
+            display: block;
+            line-height: 22px;
+            padding: 14px 16px;
+        }
+    </style>
+    <title>Reg User</title>
+@endsection
+@section('content')
+    <h1 style="text-align: center">User Registration</h1>
+    <!-- Upload  -->
+
+    <form action="reguser" method="post" enctype="multipart/form-data" >
+        {{ @csrf_field() }}
+        <div id="over" style="width:100%; height:100%">
+            <img class="img-responsive img-rounded centerImage" width="150" height="150" name="output" id="output">
+        </div>
+
+        <label class="waves-effect waves-light btn centerImage" >
+            Upload Photo
+            <input type="file" name="user_profile" value="Upload Photo" style="width: 0px;height: 0px;overflow: hidden;" onchange="loadfile(event)">
+        </label><br>
+        @error('user_profile')
+        <span style="color: red;"> {{ $message}}</span>
+        @enderror
+        <div class="row">
+            <div class="input-field col s12  l6">
+                <input type="text" name="fullname"><br>
+                @error('fullname')
+                <span style="color: red"> {{ $message}}</span>
+                @enderror
+                <label for="first_name">Full Name</label>
+            </div>
+            <div class="input-field col s12 l6">
+                <input type="text" name="email"><br>
+                @error('email')
+                <span style="color: red">{{ $message}}</span>
+                @enderror
+                <label for="last_name">Email ID</label>
+            </div>
+            <div class="input-field col s12 l6">
+                <input type="text" name="blood_group"><br>
+                @error('blood_group')
+                <span style="color: red">{{ $message}}</span>
+                @enderror
+                <label for="last_name">Blood Group</label>
+            </div>
+            <div class="input-field col s12 l6">
+                <input type="text" name="contact_number"><br>
+                @error('contact_number')
+                <span style="color: red">{{ $message}}</span>
+                @enderror
+                <label for="last_name">Contact Number</label>
+            </div>
+            <div class="input-field col s12 ">
+                <input type="text" name="gender"><br>
+                @error('gender')
+                <span style="color: red">{{ $message}}</span>
+                @enderror
+                <label for="last_name">Gender</label>
+            </div>
+            <div class="input-field col s12 l6">
+                <input type="password" name="password" ><br>
+                @error('password')
+                <span style="color: red">{{ $message}}</span>
+                @enderror
+                <label for="last_name">Password</label>
+            </div>
+            <div class="input-field col s12 l6">
+                <input type="password" name="confirm_password"><br>
+                @error('confirm_password')
+                <span style="color: red">{{ $message}}</span>
+                @enderror
+                <label for="last_name">Confirm Password</label>
+            </div>
+            <button type="submit"  class="waves-effect waves-light btn">Submit</button>
+        </div>
+
+    </form>
+    <script>
+        var loadfile= function(event)
+        {
+            var output=document.getElementById("output");
+            output.src= URL.createObjectURL(event.target.files[0]);
+        };
+    </script>
+@endsection
