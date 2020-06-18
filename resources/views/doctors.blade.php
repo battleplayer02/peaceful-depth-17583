48c81f4797610434bf4edac28cca1d5b1e42deda
Administrator <himanshu00qwerty@gmail.com>
2020-06-18 18:19:40 +0530

    my docs

--- resources/views/doctors.blade.php
+++ resources/views/doctors.blade.php
@@ -3,5 +3,74 @@
     <title>List Of Doctors</title>
 @endsection
 @section('content')
-    <h1>List Of Doctors</h1>
+    <h1 style="align-content: center">List Of Doctors</h1>
+    <!-- Team Member Section Start here -->
+    <div class="team-section bg-team padding-tb">
+        <div class="container">
+            <div class="section-header wow fadeInUp" data-wow-delay="0.3s">
+                <h2>Meet Our Best Doctors</h2>
+                <p> Dynamically formulate fully tested catalysts for change via focused methods of empowerment Assertively extend alternative synergy and extensive web services.</p>
+            </div>
+            <div class="team-area">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-xl-4 col-md-6 col-12 wow fadeInUp" data-wow-delay="0.3s">
+                        <div class="team-item">
+                            <div class="team-item-inner">
+                                <div class="team-thumb">
+                                    <img src="assets/images/team/01.jpg" alt="team-membar">
+                                </div>
+                                <div class="team-content">
+                                    <h5 class="member-name">Dorothy M. Nickell</h5>
+                                    <span class="member-dagi">Throat Specialist</span>
+                                    <p class="member-details">Proce Aran Manu Proucs Rahe Conen Cuve Manu Produ Rahe Cuvaes Mana The Conen Testin Motin Was Procedur</p>
+                                    <ul class="icon-style-list lab-ul">
+                                        <li><i class="icofont-phone"></i><span>+880 1234 567 890</span></li>
+                                        <li><i class="icofont-envelope"></i><span>d.m.nickell@gmail.com</span></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6 col-12 wow fadeInUp" data-wow-delay="0.4s">
+                        <div class="team-item">
+                            <div class="team-item-inner">
+                                <div class="team-thumb">
+                                    <img src="assets/images/team/02.jpg" alt="team-membar">
+                                </div>
+                                <div class="team-content">
+                                    <h5 class="member-name">Billie R. Courtney</h5>
+                                    <span class="member-dagi">Cardiologist</span>
+                                    <p class="member-details">Proce Aran Manu Proucs Rahe Conen Cuve Manu Produ Rahe Cuvaes Mana The Conen Testin Motin Was Procedur</p>
+                                    <ul class="icon-style-list lab-ul">
+                                        <li><i class="icofont-phone"></i><span>+880 1234 567 890</span></li>
+                                        <li><i class="icofont-envelope"></i><span>b.r.courtney@gmail.com</span></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6 col-12 wow fadeInUp" data-wow-delay="0.5s">
+                        <div class="team-item">
+                            <div class="team-item-inner">
+                                <div class="team-thumb">
+                                    <img src="assets/images/team/03.jpg" alt="team-membar">
+                                </div>
+                                <div class="team-content">
+                                    <h5 class="member-name">Courtney A. Smith</h5>
+                                    <span class="member-dagi">Rehabilitation Therapy</span>
+                                    <p class="member-details">Proce Aran Manu Proucs Rahe Conen Cuve Manu Produ Rahe Cuvaes Mana The Conen Testin Motin Was Procedur</p>
+                                    <ul class="icon-style-list lab-ul">
+                                        <li><i class="icofont-phone"></i><span>+880 1234 567 890</span></li>
+                                        <li><i class="icofont-envelope"></i><span>c.a.smith@gmail.com</span></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Team Member Section Ending here -->
+
 @stop
