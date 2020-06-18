838e840bf075310c9498102ff6e45c55f1d0d2ea
Administrator <himanshu00qwerty@gmail.com>
2020-06-18 19:08:35 +0530

    book

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -4,42 +4,38 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 @stop
 @section("content")
-    <div class="contact-us">
-        <div class="container">
-            <div class="contact-wrap">
-                <form class="contact-form">
-                    <div class="form-group row">
-                        <label for="text1" class="col-2 col-form-label">Text Field</label>
-                        <div class="col-10">
-                            <input id="text1" name="text1" type="text" class="form-control">
-                        </div>
+<div class="card shadow">
+    <form class="card-body">
+    <div class="form-group row">
+        <label for="text1" class="col-2 col-form-label">Text Field</label>
+        <div class="col-10">
+            <input id="text1" name="text1" type="text" class="form-control">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="text" class="col-2 col-form-label">Text Field</label>
+        <div class="col-10">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <div class="input-group-text">
+                        <i class="fa fa-address-card"></i>
                     </div>
-                    <div class="form-group row">
-                        <label for="text" class="col-2 col-form-label">Text Field</label>
-                        <div class="col-10">
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text">
-                                        <i class="fa fa-address-card"></i>
-                                    </div>
-                                </div>
-                                <input id="text" name="text" type="text" class="form-control">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="text2" class="col-2 col-form-label">Text Field</label>
-                        <div class="col-10">
-                            <input id="text2" name="text2" type="text" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <div class="offset-2 col-10">
-                            <button name="submit" type="submit" class="btn btn-primary">Submit</button>
-                        </div>
-                    </div>
-                </form>
+                </div>
+                <input id="text" name="text" type="text" class="form-control">
             </div>
         </div>
     </div>
+    <div class="form-group row">
+        <label for="text2" class="col-2 col-form-label">Text Field</label>
+        <div class="col-10">
+            <input id="text2" name="text2" type="text" class="form-control">
+        </div>
+    </div>
+    <div class="form-group row">
+        <div class="offset-2 col-10">
+            <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+</form>
+</div>
 @stop
