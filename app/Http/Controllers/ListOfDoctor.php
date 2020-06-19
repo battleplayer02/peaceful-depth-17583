a548bf871f4cd60a42a2fab8e70fecf258c6ad91
Administrator <himanshu00qwerty@gmail.com>
2020-06-19 15:43:33 +0530

    hos

--- app/Http/Controllers/ListOfDoctor.php
+++ app/Http/Controllers/ListOfDoctor.php
@@ -10,9 +10,17 @@ class ListOfDoctor extends Controller
     function index(){
         $data = DB::table("doctor_details")
             ->join("user_details","doctor_details.id","user_details.id")
-            ->join("hospital_details","doctor_details.hosp_id","hospital_details.id")
             ->get();
         echo "<pre>";
+        foreach ($data as $key => $value) {
+            if($key == "hosp_id"){
+                $data[$key] = array(
+                    DB::table('hospital_details')
+                    ->where('id',$value)
+                    ->get()
+                );
+            }
+        }
         print_r($data);
 //        return View("doctors",[
 //            "data"=>$data
