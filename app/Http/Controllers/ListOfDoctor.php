f64a3f1d49de942cb7efd4576d5ad048ad1ea411
Administrator <himanshu00qwerty@gmail.com>
2020-06-19 15:58:06 +0530

    adsa

--- app/Http/Controllers/ListOfDoctor.php
+++ app/Http/Controllers/ListOfDoctor.php
@@ -4,25 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
 class ListOfDoctor extends Controller
 {
     //
-    function index()
-    {
+    function index(){
         $data = DB::table("doctor_details")
-            ->join("user_details", "doctor_details.id", "user_details.id")
+            ->join("user_details","doctor_details.id","user_details.id")
             ->get();
         echo "<pre>";
-        $d = DB::table('hospital_details')
-            ->get();
-        foreach ($data as $key => $value) {
-            if ($key == "hosp_id") {
-                echo $value."<br>";
-            }
-        }
         print_r($data);
-        print_r($d);
 //        return View("doctors",[
 //            "data"=>$data
 //        ]);
