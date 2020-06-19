afc14e7973d283c9d1f3465e165beb3de5242a34
Administrator <himanshu00qwerty@gmail.com>
2020-06-19 16:00:38 +0530

    adsa

--- app/Http/Controllers/ListOfDoctor.php
+++ app/Http/Controllers/ListOfDoctor.php
@@ -11,8 +11,11 @@ class ListOfDoctor extends Controller
         $data = DB::table("doctor_details")
             ->join("user_details","doctor_details.id","user_details.id")
             ->get();
+        $hosp_data = DB::table("hospital_details")
+            ->get();
         echo "<pre>";
         print_r($data);
+        print_r($hosp_data);
 //        return View("doctors",[
 //            "data"=>$data
 //        ]);
