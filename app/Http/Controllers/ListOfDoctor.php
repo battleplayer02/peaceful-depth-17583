cc930e150cf0d390c129a587c065c61e7b3c43bd
Administrator <himanshu00qwerty@gmail.com>
2020-06-19 15:50:51 +0530

    as

--- app/Http/Controllers/ListOfDoctor.php
+++ app/Http/Controllers/ListOfDoctor.php
@@ -4,21 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 class ListOfDoctor extends Controller
 {
     //
-    function index(){
+    function index()
+    {
         $data = DB::table("doctor_details")
-            ->join("user_details","doctor_details.id","user_details.id")
+            ->join("user_details", "doctor_details.id", "user_details.id")
             ->get();
         echo "<pre>";
         foreach ($data as $key => $value) {
-            if($key == "hosp_id"){
-                $data[$key] = array(
+            if ($key == "hosp_id") {
+                $data["hospital_details"] =
                     DB::table('hospital_details')
-                    ->where('hospid',$value)
-                    ->get()
-                );
+                        ->where('hospid', $value)
+                        ->get();
+
             }
         }
         print_r($data);
