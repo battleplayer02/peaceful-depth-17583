13d9755d28a5a0987b6bb0512837ac104dd8368c
Administrator <himanshu00qwerty@gmail.com>
2020-06-19 15:37:33 +0530

    join

--- app/Http/Controllers/ListOfDoctor.php
+++ app/Http/Controllers/ListOfDoctor.php
@@ -10,6 +10,7 @@ class ListOfDoctor extends Controller
     function index(){
         $data = DB::table("doctor_details")
             ->join("user_details","doctor_details.id","user_details.id")
+            ->join("hospital_details","doctor_details.hosp_id","hospital_details.id")
             ->get();
         echo "<pre>";
         print_r($data);
