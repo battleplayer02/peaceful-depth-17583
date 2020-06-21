5aa9d899cb77265b25719953f4bb6f63a12d3342
Administrator <himanshu00qwerty@gmail.com>
2020-06-21 13:15:46 +0530

    asd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,48 +40,47 @@
         </div>
     </section>
     <!-- Banner Section ending here -->
-    <div class="corona-wrap">
-        <div class="corona-count-top">
-            <div class="row justify-content-center align-items-center">
-                <div class="col-xl-3 col-md-6 col-12">
-                    <h5>Total Corona Statistics :</h5>
-                </div>
-                <div class="col-xl-3 col-md-6 col-12">
-                    <div class="corona-item">
-                        <div class="corona-inner">
-                            <div class="corona-thumb">
-                                <img src="images/corona/01.png" alt="corona">
-                            </div>
-                            <div class="corona-content">
-                                <h3 class="count-number">{{end($data)["Confirmed"]}}</h3>
-                                <p>Active Cases</p>
-                            </div>
+git
+    <div class="corona-count-top">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-xl-3 col-md-6 col-12">
+                <h5>Total Corona Statistics :</h5>
+            </div>
+            <div class="col-xl-3 col-md-6 col-12">
+                <div class="corona-item">
+                    <div class="corona-inner">
+                        <div class="corona-thumb">
+                            <img src="images/corona/01.png" alt="corona">
+                        </div>
+                        <div class="corona-content">
+                            <h3 class="count-number">{{end($data)["Confirmed"]}}</h3>
+                            <p>Active Cases</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-3 col-md-6 col-12">
-                    <div class="corona-item">
-                        <div class="corona-inner">
-                            <div class="corona-thumb">
-                                <img src="images/corona/02.png" alt="corona">
-                            </div>
-                            <div class="corona-content">
-                                <h3 class="count-number">{{end($data)["Recovered"]}}</h3>
-                                <p>Recovered Cases</p>
-                            </div>
+            </div>
+            <div class="col-xl-3 col-md-6 col-12">
+                <div class="corona-item">
+                    <div class="corona-inner">
+                        <div class="corona-thumb">
+                            <img src="images/corona/02.png" alt="corona">
+                        </div>
+                        <div class="corona-content">
+                            <h3 class="count-number">{{end($data)["Recovered"]}}</h3>
+                            <p>Recovered Cases</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-3 col-md-6 col-12">
-                    <div class="corona-item">
-                        <div class="corona-inner">
-                            <div class="corona-thumb">
-                                <img src="images/corona/03.png" alt="corona">
-                            </div>
-                            <div class="corona-content">
-                                <h3 class="count-number">{{end($data)["Deaths"]}}</h3>
-                                <p>Deaths</p>
-                            </div>
+            </div>
+            <div class="col-xl-3 col-md-6 col-12">
+                <div class="corona-item">
+                    <div class="corona-inner">
+                        <div class="corona-thumb">
+                            <img src="images/corona/03.png" alt="corona">
+                        </div>
+                        <div class="corona-content">
+                            <h3 class="count-number">{{end($data)["Deaths"]}}</h3>
+                            <p>Deaths</p>
                         </div>
                     </div>
                 </div>
