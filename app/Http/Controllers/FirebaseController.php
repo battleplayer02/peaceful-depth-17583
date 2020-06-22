7398b32d69652caf4d409d526b83dcb6b3b488ef
Administrator <himanshu00qwerty@gmail.com>
2020-06-22 19:00:28 +0530

    a

--- app/Http/Controllers/FirebaseController.php
+++ app/Http/Controllers/FirebaseController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
 
 class FirebaseController extends Controller
 {
     public function index()
     {
 
-        $factory = (new Factory)->withServiceAccount(__DIR__.'AuthKey.json')->createDatabase();
-        $firebasedatabase = $factory->getReference("sample");
-        echo $firebasedatabase->getKey().":".$firebasedatabase->getValue();
+        $factory = (new Factory)->withServiceAccount('AuthKey.json');
+        $database = $factory->createDatabase();
+        $reference = $database->getReference('sample');
+        $snapshot = $reference->getSnapshot();
+        echo $snapshot->getKey().":".$snapshot->getValue();
+
     }
 }
