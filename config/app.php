eff7b0d297aef4be69527a83995fca4807516cfb
Administrator <himanshu00qwerty@gmail.com>
2020-06-22 18:27:54 +0530

    a

--- config/app.php
+++ config/app.php
@@ -140,7 +140,6 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
-        Kreait\Laravel\Firebase\ServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
