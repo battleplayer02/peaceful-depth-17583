fc500312409ca773805ba6be88ff821c02418e16
Administrator <himanshu00qwerty@gmail.com>
2020-06-23 20:33:50 +0530

    login

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ChatController extends Controller
 {
     //
     function index()
     {
-        return view('Chat');
+        $data = DB::table('appointments')
+            ->where('id', session('logininfo')[0]->id)
+            ->get();
+        foreach ($data as $value)
+        {
+            $value->doctorname=DB::table("doctor_details")
+                ->join("user_details", "doctor_details.id", "user_details.id")
+                ->where('docid', $value->docid)
+                ->get();
+        }
+        return view('Chat',[
+            "data"=>$data
+        ]);
     }
 }
