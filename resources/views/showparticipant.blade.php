b222324f1fd21f00f16201f46fa9825862f7cb86
gurv12345 <gp41915@gmail.com>
2020-06-25 18:24:20 +0530

    show

--- resources/views/showparticipant.blade.php
+++ resources/views/showparticipant.blade.php
@@ -5,18 +5,18 @@
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
 
     <!-- Isolated Version of Bootstrap, not needed if your site already uses Bootstrap -->
-    <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" />
+    <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css"/>
 
     <!-- Bootstrap Date-Picker Plugin -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+    <script type="text/javascript"
+            src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
     <title>Your Labs</title>
 @endsection
 
 @section('content')
-    <pre>
-        {{$participants}}
-    </pre>
+
     <div class="container">
         <br/><br/>
         <div class="section-wrapper">
@@ -28,18 +28,52 @@
                                 <h4>Your Report</h4>
                                 <table class="table table-striped">
                                     <thead>
+                                    <?php
+                                    $i = 1;
+                                    ?>
                                     <tr>
                                         <th>Sr.No</th>
-                                        <th>Issued By</th>
-                                        <th>Report</th>
+                                        <th>Participant Name</th>
+                                        <th>Participant Contact</th>
+                                        <th>Address</th>
+                                        <th>Assigned Work</th>
+                                        <th>Add Work</th>
                                     </tr>
                                     </thead>
                                     <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>Labs Lord</td>
-                                        <td><button type="submit" class="btn btn-primary">Download</button></td>
-                                    </tr>
+                                    @foreach($particpants as $value)
+                                        <?php
+                                        $i++;
+                                        ?>
+
+                                        <tr>
+                                            <th>{{$i}}</th>
+                                            <th>{{$value->pname}}</th>
+                                            <th>{{$value->pmobile}}</th>
+                                            @if($value->address == 'null')
+                                                <th>
+                                                    Work is not alloted
+                                                </th>
+                                            @else
+                                                <th>
+                                                    {{$value->address}}
+                                                </th>
+                                            @endif
+
+                                            @if($value->assigned_work == 'null')
+                                                <th>
+                                                    No work Assigned
+                                                </th>
+                                            @else
+                                                <th>
+                                                    {{$value->assigned_work}}
+                                                </th>
+                                            @endif
+                                            <th>
+                                                <button class="btn btn-primary">Edit</button>
+                                            </th>
+                                        </tr>
+                                    @endforeach
                                     </tbody>
                                 </table>
                             </div>
