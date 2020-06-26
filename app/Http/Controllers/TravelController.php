ac69e53e499ae5ef5f46a6f662e9ac008b811adc
gurv12345 <gp41915@gmail.com>
2020-06-26 18:19:01 +0530

    Travel2

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -10,26 +10,26 @@ class TravelController extends Controller
     //
     function index(Request $request)
     {
-         $passdetails=DB::table('pass')
-             ->insert([
-                 'vehicle_type'=>$request->vehicle_type,
-                 'valid_till'=>$request->ticket_valid,
-                 'reason'=>$request->reason,
-                 'vehicle_number'=>$request->vehicle_no,
-                 'ticket_number'=>session('logininfo')[0]->id
-             ]);
-         if($passdetails == 1)
-         {
-             echo
-             "<script>
+        $passdetails = DB::table('pass')
+            ->insert([
+                'vehicle_type' => $request->vehicle_type,
+                'valid_till' => $request->ticket_valid,
+                'reason' => $request->reason,
+                'vehicle_number' => $request->vehicle_no,
+                'ticket_number' => session('logininfo')[0]->id
+            ]);
+        if ($passdetails == 1) {
+            return function () {
+
+                echo
+                "<script>
                 alert('Get Your Pass Verified');
-                window.location('pass');
               </script>";
 
-         }
-         else
-         {
-             return 0;
-         }
+                return view('pass');
+            };
+        } else {
+            return 0;
+        }
     }
 }
